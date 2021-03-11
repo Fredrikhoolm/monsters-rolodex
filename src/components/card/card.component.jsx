168dd9c3d7624a7d5d013cf2ad88c0e17fea7d43
Fredrik Holm Eikrem <fredrik_eikrem@hotmail.com>
2021-03-11 14:30:00 +0100

    adding files

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+import './card.style.css';
+
+export const Card = props => (
+    <div className='card-container'>
+        <img alt="monsters" src={`https://robohash.org/${props.monsters.id}?set=set2&size=180x180`}></img>
+        <h2>{props.monsters.name}</h2>
+        <p>{props.monsters.email}</p>
+    </div>
+)
